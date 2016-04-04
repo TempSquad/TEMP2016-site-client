3518b39e1d3ed18e921df92dc0e6313d9adfe6aa
Pavel Malykh <goodwinrock777@gmail.com>
2016-04-04 18:21:04 +0300

    Greenatom site

---
+++ dist/ang-mat.js
@@ -0,0 +1,6 @@
+angular.module("angmat",['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+
+var app = angular.module("angmat",['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+	app.controller("AppCtrl", function($scope, $http) {
+
+		});
